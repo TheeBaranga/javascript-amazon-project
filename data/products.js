cb94aad6e8740486055275d14db8466547050783
Kalvin Baranga <120325260+TheeBaranga@users.noreply.github.com>
2025-10-10 12:51:21 +0300

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -62,7 +62,30 @@ console.log(date);
 console.log(date.toLocaleTimeString());
 */
 
-export const products = [
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+};
+
+//Put let instead of const to fix the variable naming issue that was conflicting with the code just above.
+
+export let products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -727,3 +750,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+
