af7f917f13bb44e72f6b34a62d195403e7d20ff2
Kalvin Baranga <120325260+TheeBaranga@users.noreply.github.com>
2025-10-12 16:12:44 +0300

    convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -64,10 +64,29 @@ console.log(date.toLocaleTimeString());
 
 export let products = [];
 
-function loadProductsFetch() {
-  fetch
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('load Products');
+  });
+
+  return promise;
 }
 
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -86,6 +105,8 @@ export function loadProducts(fun) {
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
 };
+
+
 
 /*
 export const products = [
