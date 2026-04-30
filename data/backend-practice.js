a96b282ca4fe402dc977017ea3280d2542d6312e
Kalvin Baranga <120325260+TheeBaranga@users.noreply.github.com>
2026-04-30 17:23:03 +0300

    fixed

--- data/backend-practice.js
+++ data/backend-practice.js
@@ -4,6 +4,6 @@ xhr.addEventListener('load', () => {
     console.log(xhr.response);
 });
 
-xhr.open('GET', 'https://supersimplebackend.dev');
+xhr.open('GET', 'https://supersimplebackend.dev/products/first');
 xhr.send();
 xhr.response
